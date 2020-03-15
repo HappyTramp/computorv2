cc2593028c5f380e177adbf8905a43d665ac64cf
Charles <sircharlesaze@gmail.com>
2020-03-15 14:34:31 +0100

    atom module with rational and imaginary types and operations

--- src/matrix.hs
+++ src/matrix.hs
@@ -1,10 +1,3 @@
 module Matrix where
 
--- import Data.List
 
--- newtype Matrix a = Matrix { getMatrix :: [MatrixRow a] }
--- type MatrixRow a = [a]
-
--- instance Show a => Show (Matrix a) where
---     show (Matrix m) = intercalate "\n" (map showLine m)
---         where showLine l = "[ " ++ intercalate " , " (map show l) ++ " ]"
